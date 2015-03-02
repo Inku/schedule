419a9c5c061d464c24c2b3c1ad8f5dfb28d962f8
Inku <shenglingmaque@gmail.com>
2015-03-02 23:32:55 +0800

    0802

---
+++ src/com/ifeng/array/ArrayTool.java
@@ -0,0 +1,59 @@
+package com.ifeng.array;
+
+/**
+ * Created by Inku on 2015/3/2.
+ */
+public class ArrayTool {
+
+    private ArrayTool() {
+    }
+
+    /**
+     * 获取整形数组最大值。
+     */
+    public static int getMax(int[] arr) {
+        int maxIndex = 0;
+        for (int x = 1; x < arr.length; x++) {
+            if (arr[x] > arr[maxIndex])
+                maxIndex = x;
+        }
+
+        return arr[maxIndex];
+    }
+
+    public void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public int getIndex(int[] arr, int key) {
+        for (int x = 0; x < arr.length; x++) {
+            if (arr[x] == key)
+                return x;
+        }
+        return -1;
+    }
+
+    public String arrayToString(int[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length; i++) {
+            if (i != arr.length - 1) {
+                str = str + arr[i] + ",";
+            } else {
+                str = str + arr[i] + "]";
+            }
+        }
+        return str;
+    }
+}
---
+++ src/com/ifeng/array/ArrayToolDemo.java
@@ -0,0 +1,11 @@
+package com.ifeng.array;
+
+/**
+ * Created by Inku on 2015/3/2.
+ */
+public class ArrayToolDemo {
+    public static void main(String[] args) {
+        int[] arr = {4, 8, 2, 9, 7, 6};
+        System.out.println(ArrayTool.getMax(arr));
+    }
+}
