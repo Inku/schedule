cfca698851cdfb722861cb52d4c9b328de77cd39
Inku <inku@inku.me>
2015-10-12 10:20:41 +0800

    commit

--- src/main/java/com/ifeng/schedule/thread/Resource.java
+++ src/main/java/com/ifeng/schedule/thread/Resource.java
@@ -1,5 +1,6 @@
 package com.ifeng.schedule.thread;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,18 +13,20 @@ public class Resource {
     private int count = 1;
     private boolean flag = false;
     private Lock lock = new ReentrantLock();
+    private Condition in = lock.newCondition();
+    private Condition out = lock.newCondition();
 
     public void set(String name) {
         lock.lock();
         try {
             while (flag) {
-                wait();
+                in.await();
             }
             this.name = name + count;
             count++;
             System.out.println(Thread.currentThread().getName() + "-->生产者-->" + this.name);
             flag = true;
-            notifyAll();
+            out.signal();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -35,11 +38,11 @@ public class Resource {
         lock.lock();
         try {
             while (!flag) {
-                wait();
+                out.await();
             }
             System.out.println(Thread.currentThread().getName() + "-->消费者----->" + this.name);
             flag = false;
-            notifyAll();
+            in.signal();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
